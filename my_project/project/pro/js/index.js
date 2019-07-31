2791177a272005d88e0d9186f5212ef8cc3e57e8
zhangxudong <yi4323-xiaoran@foxmail.com>
2019-07-31 21:03:30 +0800

    Update header.html

--- my_project/project/pro/js/index.js
+++ my_project/project/pro/js/index.js
@@ -1,9 +1,13 @@
 $("#box1").hover(function(){
 			$("#box2").toggle();
-})
+});
 $("#box2").mouseenter(function(){
 	$("#box2").css("display","block")
-})
+});
 $("#box2").mouseleave(function(){
 	$("#box2").css("display","none")
+});
+$("#buts1").click(function(){
+	$("#my_ipt").removeClass("show");
+	if($("#ssou").has("d-sm-none"))
 })
