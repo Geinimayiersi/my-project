b7a056fd6e1a44dfc1b25d70e745e4c626bd8d49
zhangxudong <yi4323-xiaoran@foxmail.com>
2019-08-02 23:54:16 +0800

    update header.html

--- my_project/project/pro/js/index.js
+++ my_project/project/pro/js/index.js
@@ -1,13 +1,29 @@
-$("#box1").hover(function(){
-			$("#box2").toggle();
-});
-$("#box2").mouseenter(function(){
-	$("#box2").css("display","block")
-});
-$("#box2").mouseleave(function(){
-	$("#box2").css("display","none")
-});
-$("#buts1").click(function(){
-	$("#my_ipt").removeClass("show");
-	$("#ssuo").toggle();
+$(function(){
+	//搜索推荐鼠标悬停block
+	$("#box1").hover(function(){
+				$("#box2").toggle();
+	});
+	//搜索鼠标进入block
+	$("#box2").mouseenter(function(){
+		$("#box2").css("display","block")
+	});
+	//搜索鼠标离开none
+	$("#box2").mouseleave(function(){
+		$("#box2").css("display","none")
+	});
+	//jquery sm搜索点击 shou显示并隐藏
+	$("#buts1").click(function(){
+		$("#my_ipt").removeClass("show");
+		$("#ssuo").toggle();
+	});
+	/*头部fixed */
+	function changeHeight(){
+		var h=$(".header").css("height");
+		$(".mh").css({"height":h});
+	}
+	$(window).resize(changeHeight);
+	// 页面加载完成时调用获取高度
+	changeHeight();
 })
+
+
